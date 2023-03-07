7b9ff43e8ad2ad1ade4ac0fb655c3d1c1df3a3d9
Morienzo <morienzo@hotmail.fr>
2023-03-07 01:10:18 +0100

    Release 1.0.1

--- app/src/main/java/xyz/blockside/idenawebapp/MainActivity.java
+++ app/src/main/java/xyz/blockside/idenawebapp/MainActivity.java
@@ -2,9 +2,7 @@ package xyz.blockside.idenawebapp;
 
 
 import android.app.Activity;
-import android.content.res.Configuration;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -13,29 +11,38 @@ public class MainActivity extends Activity {
 
     private WebView mWebView;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getResources().getConfiguration().orientation ==
-                Configuration.ORIENTATION_PORTRAIT) {
-            setContentView(R.layout.activity_main);
-        } else {
-            setContentView(R.layout.activity2_main);
-        }
+        setContentView(R.layout.activity_main);
+
 
         mWebView = (WebView) findViewById(R.id.activity_main_webview);
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
         webSettings.setDomStorageEnabled(true);
-        mWebView.loadUrl("https://app.idena.io/");
+        mWebView.loadUrl("https://app.idena.io");
+        mWebView.reload();
+
         mWebView.setWebViewClient(new MyAppWebViewClient() {
             @Override
             public void onPageFinished(WebView view, String url) {
-                findViewById(R.id.imageLoading1).setVisibility(View.GONE);
+
                 findViewById(R.id.activity_main_webview).setVisibility(View.VISIBLE);
+
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+
+                findViewById(R.id.imageLoading1).setVisibility(View.GONE);
+
             }
         });
+
     }
 
 
